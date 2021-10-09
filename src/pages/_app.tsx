4346fadfa4513c8ad8a2265bd05f451912a308bd
Marco Rezende <rezende@espressolabs.com.br>
2021-10-09 17:02:41 -0300

    theme e _app: definido tema

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,11 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { MainSideBar } from "../components/common/SideBar";
 import { routes } from "../router";
+import theme from "../config/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <MainSideBar routes={routes}>
         <Component {...pageProps} />
       </MainSideBar>
