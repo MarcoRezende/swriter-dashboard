9332506ed33c7a47d66bd80cfdf6950809193457
Marco Rezende <rezende@espressolabs.com.br>
2021-10-09 19:04:17 -0300

    useHint: criado hook useHint

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,7 +1,10 @@
 import { CategoryProvider } from "./useCategory";
+import { HintProvider } from "./useHint";
 
 const AppProvider: React.FC = ({ children }) => (
-  <CategoryProvider>{children}</CategoryProvider>
+  <CategoryProvider>
+    <HintProvider>{children}</HintProvider>
+  </CategoryProvider>
 );
 
 export default AppProvider;
---
+++ src/hooks/useHint.tsx
@@ -0,0 +1,42 @@
+import { createContext, useContext } from "react";
+
+import { Hint } from "../models/Hint";
+import { createOneBase } from "../services/common";
+import { HintResource } from "../services/hint";
+
+export interface HintAPI {
+  createOne(HintDto: Hint): Promise<Hint>;
+}
+
+export const HintContext = createContext<HintAPI>({} as HintAPI);
+
+export const HintProvider: React.FC = ({ children }) => {
+  const createOne = async (hintDto: Hint): Promise<Hint> => {
+    const hint = new Hint(hintDto);
+
+    return (
+      (
+        await createOneBase<Hint>({
+          resource: HintResource,
+          data: hint,
+        })
+      ).data || {}
+    );
+  };
+
+  return (
+    <HintContext.Provider value={{ createOne }}>
+      {children}
+    </HintContext.Provider>
+  );
+};
+
+export const useHint = (): HintAPI => {
+  const context = useContext(HintContext);
+
+  if (!context) {
+    throw new Error("useHint must be within a HintContext");
+  }
+
+  return context;
+};
