f21f0d3e455736bd24824aa630366c34ea68b4ba
Marco Rezende <rezende@espressolabs.com.br>
2021-10-09 22:12:28 -0300

    Table: adicionado botão para criação ; adicionada coluna para número da coluna

--- src/components/common/Table.tsx
+++ src/components/common/Table.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+
+import { Button } from "@chakra-ui/button";
+import { Divider, Heading } from "@chakra-ui/layout";
 import {
   Table as ChakraTable,
   TableProps as ChakraTableProps,
@@ -9,6 +13,8 @@ import {
 } from "@chakra-ui/table";
 
 interface TableProps extends ChakraTableProps {
+  title: string;
+  resource: string;
   columns: string[];
   columnsContent: Array<{
     id: string;
@@ -17,6 +23,8 @@ interface TableProps extends ChakraTableProps {
 }
 
 export const Table: React.FC<TableProps> = ({
+  title,
+  resource,
   columns,
   columnsContent,
   ...rest
@@ -24,39 +32,61 @@ export const Table: React.FC<TableProps> = ({
   const hasContent: boolean = !!columnsContent.length;
 
   return (
-    <ChakraTable {...rest} variant="striped">
-      <Thead>
-        <Tr>
-          {columns.map((column) => (
-            <Th key={"tr-" + column}>{column}</Th>
-          ))}
-        </Tr>
-      </Thead>
-      <Tbody>
-        {hasContent ? (
-          columnsContent.map((content) => (
-            <Tr key={"row" + content.id}>
-              {content.values.map((value) => (
-                <Td key={"td" + value} py="1rem" px="2rem">
-                  {value}
-                </Td>
-              ))}
-            </Tr>
-          ))
-        ) : (
+    <>
+      <Button
+        borderColor="blue.800"
+        px="4rem"
+        mb="1rem"
+        d="block"
+        ml="auto"
+        variant="outline"
+        _hover={{
+          bg: "blue.800",
+          color: "white",
+        }}
+      >
+        <Link href={`/criar/${resource}`}>Criar</Link>
+      </Button>
+      <Heading mb="0.5rem">{title}.</Heading>
+      <Divider mb="2rem" />
+      <ChakraTable {...rest} variant="striped">
+        <Thead>
           <Tr>
-            <Td
-              py="1rem"
-              px="2rem"
-              colSpan={columns.length}
-              w="100%"
-              textAlign="center"
-            >
-              Sem conteúdo
-            </Td>
+            <Th>Número</Th>
+            {columns.map((column) => (
+              <Th key={"tr-" + column}>{column}</Th>
+            ))}
           </Tr>
-        )}
-      </Tbody>
-    </ChakraTable>
+        </Thead>
+        <Tbody>
+          {hasContent ? (
+            columnsContent.map((content) => (
+              <Tr key={"row" + content.id}>
+                {content.values.map((value, index) => (
+                  <>
+                    <Td>{index + 1}</Td>
+                    <Td key={"td" + value} py="1rem" px="2rem">
+                      {value}
+                    </Td>
+                  </>
+                ))}
+              </Tr>
+            ))
+          ) : (
+            <Tr>
+              <Td
+                py="1rem"
+                px="2rem"
+                colSpan={columns.length + 1}
+                w="100%"
+                textAlign="center"
+              >
+                Sem conteúdo
+              </Td>
+            </Tr>
+          )}
+        </Tbody>
+      </ChakraTable>
+    </>
   );
 };
