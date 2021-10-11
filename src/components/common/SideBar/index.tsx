5793fccee986bb9d8f6e56b1ffec574e46e23521
Marco Rezende <rezende@espressolabs.com.br>
2021-10-11 08:41:40 -0300

    SideBar: altura alterada

--- src/components/common/SideBar/index.tsx
+++ src/components/common/SideBar/index.tsx
@@ -21,7 +21,7 @@ export const MainSideBar: React.FC<MainSideBarProps> = ({
 }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
+    <Box h="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
       <SidebarContent
         routes={routes}
         onClose={() => onClose}
@@ -41,7 +41,7 @@ export const MainSideBar: React.FC<MainSideBarProps> = ({
         </DrawerContent>
       </Drawer>
       <MobileNav onOpen={onOpen} />
-      <Box ml={{ base: 0, md: 60 }} p="4">
+      <Box ml={{ base: 0, md: 60 }} h="100%" p="4">
         {children}
       </Box>
     </Box>
