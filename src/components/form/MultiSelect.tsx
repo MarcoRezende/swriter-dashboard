59a5518df9939be85b3d074968375131548bbabf
Marco Rezende <rezende@espressolabs.com.br>
2021-10-16 22:33:06 -0300

    MultiSelect: adicionado componente

---
+++ src/components/form/MultiSelect.tsx
@@ -0,0 +1,30 @@
+import { Select } from "chakra-react-select";
+import { ChakraSelectProps } from "chakra-react-select/dist/types/types";
+
+export type MultiSelectOption = {
+  label: string;
+  value: string;
+  isFixed: boolean;
+};
+
+export interface MultiSelectProps extends ChakraSelectProps {
+  options?: MultiSelectOption[];
+}
+
+export const MultiSelect: React.FC<MultiSelectProps> = ({
+  options,
+  noOptionsMessage,
+  children,
+  placeholder,
+}) => {
+  return (
+    <Select
+      closeMenuOnSelect={false}
+      colorScheme="blue"
+      isMulti
+      options={options}
+      placeholder={placeholder}
+      noOptionsMessage={noOptionsMessage}
+    />
+  );
+};
