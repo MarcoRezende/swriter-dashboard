3cf1297d949393f594d1557002286a167aa61ecf
marcorezende <marcorezende@outlook.com.br>
2021-10-31 02:54:42 -0300

    definida página de edição das dicas

---
+++ src/pages/hint/edit/[hintId].tsx
@@ -0,0 +1,88 @@
+import { useRouter } from "next/router";
+import { memo, useEffect, useState } from "react";
+
+import {
+  CreateForm,
+  FieldType,
+  FormField,
+} from "../../../components/form/Create";
+import { optionsFormatter } from "../../../components/form/Select";
+import { Theme } from "../../../models/Theme";
+import { categoryResource } from "../../../services/category";
+import { hintResource } from "../../../services/hint";
+import { getManyBase } from "../../../services/common";
+
+const EditField = () => {
+  const [categories, setCategories] = useState<Theme[]>([]);
+  const router = useRouter();
+  const hintId = router.query.hintId as string | undefined;
+
+  useEffect(() => {
+    let cancel = false;
+
+    const fetchData = async () => {
+      try {
+        const fetchedCategories = await getManyBase<Theme>({
+          resource: categoryResource,
+        });
+
+        if (cancel) return;
+
+        setCategories(fetchedCategories);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      cancel = true;
+    };
+  }, [hintId]);
+
+  const fields: FormField[] = [
+    {
+      name: "tip",
+      label: "Dica",
+      placeholder: "frase ou palavra",
+      type: FieldType.TEXTAREA,
+      rules: { required: true },
+    },
+    {
+      name: "book",
+      label: "Livro",
+      placeholder: "obra",
+      type: FieldType.TEXT,
+      rules: {},
+    },
+    {
+      name: "author",
+      label: "Autor",
+      placeholder: "criador da obra",
+      type: FieldType.TEXT,
+      rules: {},
+    },
+    {
+      name: "categories",
+      label: "Categorias",
+      placeholder: "selecione",
+      type: FieldType.MULTI_SELECT,
+      selectOptions: optionsFormatter(categories, "name"),
+      rules: { required: true },
+      selectOptionKey: "name",
+    },
+  ];
+
+  return (
+    <CreateForm
+      mode="edit"
+      title="sentença"
+      endpoint={hintResource}
+      fields={fields}
+      idName="hintId"
+    />
+  );
+};
+
+export default memo(EditField);
