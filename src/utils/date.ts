174185977e0972ae0482793354337c9e1246a7d6
marco <marcorezende@outlook.com.br>
2021-12-05 21:21:18 -0300

    date: definida função para converter data

---
+++ src/utils/date.ts
@@ -0,0 +1,34 @@
+import { parseISO, formatRelative, subHours } from "date-fns";
+import { pt } from "date-fns/locale";
+import { format } from "date-fns-tz";
+
+const TIMEZONE_OFFSET = 3;
+
+interface FormatOptions {
+  relative?: boolean;
+  replacerOnInvalid?: string;
+  format?: string;
+}
+
+export const formatDate = (
+  date: string | Date | undefined | null,
+  options?: FormatOptions
+): string => {
+  if (!date) return options?.replacerOnInvalid || "-";
+
+  if (options?.relative) {
+    const [createAt, now] = [
+      subHours(new Date(date), TIMEZONE_OFFSET),
+      subHours(new Date(), TIMEZONE_OFFSET),
+    ];
+
+    const formattedDate = formatRelative(createAt, now, {
+      locale: pt,
+    });
+    return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+  }
+
+  return format(parseISO(String(date)), options?.format ?? "dd/MM/yy", {
+    timeZone: "America/Sao_Paulo",
+  });
+};
