fbc3a794523fdea2d5dbcaba014e11a48797cc82
marco <marcorezende@outlook.com.br>
2022-01-09 13:27:40 -0300

    _app: adicionado providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,29 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from 'next/app';
 
-import { MainSideBar } from "../components/common/SideBar";
-import theme from "../config/theme";
-import { routes } from "../router";
+import { MainSideBar } from '../components/common/SideBar';
+import theme from '../config/theme';
+import { routes } from '../router';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { AppProvider } from '../hooks';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <MainSideBar routes={routes}>
-        <Component {...pageProps} />
-      </MainSideBar>
-    </ChakraProvider>
+    <QueryClientProvider client={client}>
+      <AppProvider>
+        <ChakraProvider theme={theme}>
+          <MainSideBar routes={routes}>
+            <Component {...pageProps} />
+          </MainSideBar>
+        </ChakraProvider>
+      </AppProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 export default MyApp;
