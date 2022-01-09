5ffe4974fdf3533499f54f2b234461019f7e61a3
marco <marcorezende@outlook.com.br>
2022-01-09 16:08:22 -0300

    hook de entity: configurado cache das opções do select

--- src/hooks/entity.tsx
+++ src/hooks/entity.tsx
@@ -3,14 +3,21 @@ import { useQuery, UseQueryResult } from 'react-query';
 
 import { RequestQueryBuilder } from '@nestjsx/crud-request';
 
+import { FormField } from '../components/form/EntityField';
 import { CrudModel } from '../models/crud.model';
 import { formatTableContent, TableColumnsProps } from '../utils/table';
+import { optionsFormatter } from '../components/form/fields/BaseSelect';
 
 interface EntityContextData {
   generateTableContent(
     model: CrudModel<any>,
     columns: string[]
   ): UseQueryResult<TableColumnsProps, unknown>;
+
+  loadRelationOptions(
+    model: CrudModel<any>,
+    formFields: string[]
+  ): UseQueryResult<FormField[], unknown>;
 }
 
 const EntityContext = createContext<EntityContextData>({} as EntityContextData);
@@ -23,17 +30,85 @@ const requestQuery = RequestQueryBuilder.create().sortBy({
 export const EntityProvider: React.FC = ({ children }) => {
   const generateTableContent = (model: CrudModel<any>, columns: string[]) => {
     async function fetchData(): Promise<TableColumnsProps> {
-      const localEntityDescription = await model.entityDescription();
+      const entityDescription = await model.entityDescription();
       const foundEntities: any[] = await model.getMany(requestQuery);
 
-      return formatTableContent(columns, localEntityDescription, foundEntities);
+      return formatTableContent(columns, entityDescription, foundEntities);
     }
 
-    return useQuery(model.endpoint, fetchData);
+    return useQuery(model.endpoint, fetchData, {
+      staleTime: 5 * 500, // 5s
+    });
+  };
+
+  const loadRelationOptions = (model: CrudModel<any>, formFields: string[]) => {
+    const fetchData = async (): Promise<FormField[]> => {
+      const entityDescription = await model.entityDescription();
+      const relationOptions = await model.loadRelationOptions();
+
+      const formattedFields = formFields.reduce<FormField[]>(
+        (allFields: FormField[], key) => {
+          const description = entityDescription.find(
+            (desc) => desc.key === key
+          );
+
+          if (description?.type) {
+            const {
+              placeholder,
+              subject: label,
+              type,
+              rules = {},
+              key: name,
+              relation,
+              selectKey = 'name',
+            } = description;
+
+            const fieldProps = {
+              placeholder: placeholder ?? '',
+              label,
+              type,
+              name,
+              rules,
+            };
+
+            if (relation) {
+              const selectOptions =
+                relationOptions.find((relation) => relation.key === key)
+                  ?.data ?? [];
+
+              if (!selectKey) {
+                console.warn(
+                  'Using default key (name) to generate select values.'
+                );
+              }
+
+              Object.assign(fieldProps, {
+                selectKey,
+                selectOptions: optionsFormatter(selectOptions, selectKey),
+              });
+            }
+
+            allFields.push(fieldProps);
+          }
+
+          return allFields;
+        },
+        []
+      );
+
+      return formattedFields;
+    };
+
+    return useQuery(model.endpoint + '-relations', fetchData, {
+      refetchOnWindowFocus: false,
+      staleTime: 5 * 5000, // 5m
+    });
   };
 
   return (
-    <EntityContext.Provider value={{ generateTableContent }}>
+    <EntityContext.Provider
+      value={{ generateTableContent, loadRelationOptions }}
+    >
       {children}
     </EntityContext.Provider>
   );
